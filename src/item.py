edef2e32b16b67e1e26982ab4dfbefcf57d2bb19
jamescross813 <crossjames813@gmail.com>
2023-09-14 14:30:34 -0500

    innit method added

--- src/item.py
+++ src/item.py
@@ -1,3 +1,9 @@
 #class describing items in world and on characters
 class Item:
-    pass
+    def __init__(self, name, description, type, hitpoints, health, defense):
+        self.name = name
+        self.description = description
+        self.type = type
+        self.hitpoints = hitpoints
+        self.health = health
+        self.defense = defense
