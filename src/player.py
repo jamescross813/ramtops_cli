0144366bc0c9ff035396301d531bff26a6171a40
jamescross813 <crossjames813@gmail.com>
2023-09-14 14:19:22 -0500

    innit method added

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 class Player:
-    pass
+    def __innit__(self, name, gender):
+        self.name = name
+        self.gender = gender
+        self.hp = 100
+
+#player class for playable characters
