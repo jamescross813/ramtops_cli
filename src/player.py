8cb2ded40d3cb827737348100b7e2e7e343fceea
jamescross813 <crossjames813@gmail.com>
2023-09-14 14:19:48 -0500

    comment formatting

--- src/player.py
+++ src/player.py
@@ -1,7 +1,6 @@
+#player class for playable characters
 class Player:
     def __innit__(self, name, gender):
         self.name = name
         self.gender = gender
         self.hp = 100
-
-#player class for playable characters
