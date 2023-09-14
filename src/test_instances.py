9d7fe1061339c503eaf506c483b5ec7ffae30aac
jamescross813 <crossjames813@gmail.com>
2023-09-14 14:55:34 -0500

    npc instance test

--- src/test_instances.py
+++ src/test_instances.py
@@ -5,3 +5,6 @@ new_player = PlayerCharacter("Bob", "male")
 
 print(new_player.name)
 
+new_npc = Npc("Fish", "Fighter", 300, "Cute and fluffy on the outside, but those puppy dog eyes will defeat all")
+
+print(new_npc.name)
