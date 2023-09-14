9d2efdd21d29b5227a9fbe96b7655e7207eb38f1
jamescross813 <crossjames813@gmail.com>
2023-09-14 14:59:54 -0500

    item and location instance tests

--- src/test_instances.py
+++ src/test_instances.py
@@ -1,10 +1,16 @@
 from player_character import PlayerCharacter
 from npc import Npc
+from location import Location
+from item import Item
 
 new_player = PlayerCharacter("Bob", "male")
-
 print(new_player.name)
 
 new_npc = Npc("Fish", "Fighter", 300, "Cute and fluffy on the outside, but those puppy dog eyes will defeat all")
-
 print(new_npc.name)
+
+new_location = Location("Lancre Castle", (0,5), "Castle of the Lancre monarch")
+print(new_location.coordinates)
+
+new_item = Item("Bread", "some bread", "health", 1, 15, 1)
+print(new_item.name)
