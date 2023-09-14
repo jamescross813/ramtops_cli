264a9c042e3d56e8d8ecf30e331e7a8fd72559cc
jamescross813 <crossjames813@gmail.com>
2023-09-14 14:23:26 -0500

    innit method added

--- src/npc.py
+++ src/npc.py
@@ -1,3 +1,7 @@
 #general non-playable character class
 class npc:
-    pass
+    def __init__(self, name, type, hp, description):
+        self.name = name
+        self.type = type
+        self.hp = hp
+        self.description = description
