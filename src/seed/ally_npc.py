4f5ca4472fea2d36f0955649f8149d566d794c7d
jamescross813 <crossjames813@gmail.com>
2023-09-15 22:33:40 -0500

    granny update

---
+++ src/seed/ally_npc.py
@@ -0,0 +1,6 @@
+from npc import Npc
+
+granny = Npc("Granny Weatherwax", "Ally", 250, "Tall, thin with a wart-free complexion and good teeth, (despite her best efforts,) and perpetual scowl")
+nanny 
+magrat
+tiffany
