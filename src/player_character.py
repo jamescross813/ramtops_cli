d21810fbbf5e4e89cf61c8fd36424d0c1fa29e67
jamescross813 <crossjames813@gmail.com>
2023-09-16 23:07:52 -0500

    repr method

--- src/player_character.py
+++ src/player_character.py
@@ -4,6 +4,9 @@ class PlayerCharacter:
         self.name = name
         self.gender = gender
         self.hp = 100
+    
+    def __repr__(self):
+        return "I am {0}, a {1}. My journey through the ramptops has left me with {2} health".format(self.name, self.gender, self.hp)
 
     #def move
 
