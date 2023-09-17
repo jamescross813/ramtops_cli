69120837d3766b61419df56741071344b73cf7cc
jamescross813 <crossjames813@gmail.com>
2023-09-16 23:17:55 -0500

    repr method addeD

--- src/npc.py
+++ src/npc.py
@@ -5,6 +5,10 @@ class Npc:
         self.type = type
         self.hp = hp
         self.description = description
+    
+    def __repr_(self):
+        return "This is {0}, an {1}. {2}. Currently health: {3}".format(self.name, self.type, self.description, self.hp)
+
 
     #def fight
 
