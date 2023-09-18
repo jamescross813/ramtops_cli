affbc7129ef61a9808d3f7d6a3c37fe4d2278636
jamescross813 <crossjames813@gmail.com>
2023-09-17 22:54:28 -0500

    north method

--- src/location.py
+++ src/location.py
@@ -8,5 +8,12 @@ class Location:
     def __repr__(self):
         return "Welcome to {0}, {2}. Your current coordinates are {2}".format(self.name, self.description, self.coordinates)
     
-    def location(self):
+    def locate_player(self):
         return "{0}: {1}".format(self.name, self.coordinates)
+    
+    def north(self):
+        current_location = self.coordinates
+        self.coordinates[0] = current_location[0]+1
+        return self.coordinates
+    
+   
