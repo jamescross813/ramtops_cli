dd17cbb4d4b46221e2043a90b73122b598d79cf8
jamescross813 <crossjames813@gmail.com>
2023-09-17 22:45:25 -0500

    typo fix

--- src/item.py
+++ src/item.py
@@ -10,7 +10,7 @@ class Item:
 
     def __repr__(self):
         return "A {0}. {1} Using this will give you: Health {2}, Attack {3}, Defense {4}.".format(self.name, self.description, self.health, self.hitpoints, self.defense)
-    
+
     #when health item is used can not be used for health again
     def used(self):
         if self.type == 'health':
