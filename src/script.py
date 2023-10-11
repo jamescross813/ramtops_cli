03968b36f37e13192d0fa0034f130a0cf4c0c32a
jamescross813 <crossjames813@gmail.com>
2023-10-10 21:51:33 -0500

    notes for script running order

---
+++ src/script.py
@@ -0,0 +1,6 @@
+#import player, npc, item, location
+
+#ask what player wants to do
+#check input is allowed
+#send to appropriate function(swtich statement)
+#if location changes, check for items and enemies--> give choise to pick up/fight respectively
