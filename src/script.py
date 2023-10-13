dc48b57ea856694a5aabc770d4fd2310def422a2
jamescross813 <crossjames813@gmail.com>
2023-10-12 21:30:19 -0500

    typo fix

--- src/script.py
+++ src/script.py
@@ -3,4 +3,4 @@
 #ask what player wants to do
 #check input is allowed
 #send to appropriate function(swtich statement)
-#if location changes, check for items and enemies--> give choise to pick up/fight respectively
+#if location changes, check for items and enemies--> give choice to pick up/fight respectively
