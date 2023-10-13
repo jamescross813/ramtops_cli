992a79daaca6a0301fbf0c774b5714d85ef8b7a7
jamescross813 <crossjames813@gmail.com>
2023-10-12 21:32:26 -0500

    additional notes to work on

--- src/npc.py
+++ src/npc.py
@@ -6,12 +6,14 @@ class Npc:
         self.hp = hp
         self.description = description
     #needs a location
+    #need attack points
+    #link to item drop?
     def __repr_(self):
         return "This is {0}, an {1}. {2}. Currently health: {3}".format(self.name, self.type, self.description, self.hp)
 
 
     #def fight
-
+        #linked to player fight
     #def speak
-    
+        #need to provide basic lines for chat to player
     
