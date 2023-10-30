840cccfb9f5d050f9febc0bbc835374664ef4b1e
jamescross813 <crossjames813@gmail.com>
2023-10-30 15:17:58 -0500

    bug hunt, not currently working, file move attempted to fix

--- src/seed/enemy_seed.py
+++
@@ -1,7 +0,0 @@
-from npc import Npc
-
-queen = Npc("Queen of the Elves", "Fighter", 200, "Tall, slim dark-haired woman, wearing a dress of red, white and black, the Queen of the Elves can appear in whatever form she likes, and no stated appearance is definitive. She wears a copper crown in her hair and has exquisitely thin hands",)
-vampire = Npc("Vlad de Magpyr", "Fighter", 200, "A clever, powerful vampire")
-frost_guy = Npc("Wintersmith", "Fighter", 150, "The creator of winter")
-boggart = Npc("River Boggart", "Fighter", 75, "A waterbased creature ready to eat the young and weak who linger on the shore.")
-hiver = Npc("The Hiver", "Fighter", 200, "A parasite, it takes over the mind and body of other creatures")
