2d196af772ce66d75a45631b9b77ccb5f8ac92cc
jamescross813 <crossjames813@gmail.com>
2023-10-31 22:56:25 -0500

    tidy up code

--- src/script.py
+++ src/script.py
@@ -1,8 +1,7 @@
 #import player, npc, item, location
 from location import Location
-import location_seed
 
-print(loc6)
+
 #ask what player wants to do
 #check input is allowed
 #send to appropriate function(swtich statement)
