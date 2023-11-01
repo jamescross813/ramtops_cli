a75331b0ca9882108f2d440adbededdfe8621324
jamescross813 <crossjames813@gmail.com>
2023-10-31 23:03:38 -0500

    ally seeds added to npc file

--- src/npc.py
+++ src/npc.py
@@ -24,3 +24,7 @@ frost_guy = Npc("Wintersmith", "Fighter", 150, "The creator of winter")
 boggart = Npc("River Boggart", "Fighter", 75, "A waterbased creature ready to eat the young and weak who linger on the shore.")
 hiver = Npc("The Hiver", "Fighter", 200, "A parasite, it takes over the mind and body of other creatures")
 #allies
+granny = Npc("Granny Weatherwax", "Ally", 250, "Tall, thin with a wart-free complexion and good teeth, (despite her best efforts,) and perpetual scowl")
+nanny = Npc("Nanny Ogg", "Ally", 225, "A dumpy and bandy-legged old woman with a face like an apple that's been left too long and an expression of near-terminal good nature")
+magrat = Npc("Magrat Garlik", "Ally", 200, "Thin, with a frequently red nose and an unruly mass of frizz and split ends")
+tiffany = Npc("Tiffany Aching", "Ally", 150, "Short, slim, brown-haired, wears big boots. Almost never wears black (except for the hat) preferring blue or green")
