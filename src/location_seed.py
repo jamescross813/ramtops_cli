3c2a0b0cda7ee8a4e5d2d98d00e3a87d1fa1fdb3
jamescross813 <crossjames813@gmail.com>
2023-10-31 22:50:25 -0500

    working location seed file

--- src/location_seed.py
+++ src/location_seed.py
@@ -1,4 +1,4 @@
-import location as Location
+from location import Location
 
 
 loc1 = Location("Mad Stoat", (1,2), "a place")
@@ -11,3 +11,4 @@ loc6 = Location("Ramtops", (1,1), "Turn back")
 loc7 = Location("Ramtops", (1,3), "Turn back")
 loc8 = Location("Ramtops", (1,2), "Turn back")
 loc9 = Location("Ramtops", (3,3), "Turn back")
+
