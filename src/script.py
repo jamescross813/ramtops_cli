8296579a6e69e110c0c76cfd81d49f0bcab180d2
jamescross813 <crossjames813@gmail.com>
2023-11-01 22:18:05 -0500

    starting script class

--- src/script.py
+++ src/script.py
@@ -4,6 +4,17 @@ from player_character import PlayerCharacter
 from npc import Npc
 from item import Item
 
+import click
+
+class Cli:
+    @click.command()
+    @click.option("--name", prompt="Welcome to the Ramptops! We're happy to have you here, but what's your name traveller?", help="The name of the user")
+    
+    def hello(name):
+        click.echo(f"Hello {name}!")
+        
+    hello()
+
 #ask what player wants to do
 #check input is allowed
 #send to appropriate function(swtich statement)
