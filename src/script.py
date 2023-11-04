2b4fde4484be6292dc41720cedb529b1012aeb93
jamescross813 <crossjames813@gmail.com>
2023-11-03 21:33:00 -0500

    wip check func

--- src/script.py
+++ src/script.py
@@ -7,6 +7,7 @@ from item import Item
 import click
 
 class Cli:
+    temp_loc ={}
     def welcome():
         name = input("Welcome to the Ramptops! We're happy to have you here, but what's your name traveller?")
         Cli.hello(name)
@@ -16,6 +17,11 @@ class Cli:
 
     #def check- where you are- check for allies and enemies and items--> give appropriate options
 
+    def check():
+        if PlayerCharacter.current_location() == False:
+            temp_loc ={0,0}
+        else:
+            Location.__repr__()
     #def options: what do you want to do
 
     
