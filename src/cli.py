dc8b3d77201684541d6f31cdcf4754d7ff4d30e7
jamescross813 <crossjames813@gmail.com>
2024-01-08 21:42:51 -0600

    cli class code moved to seperate folder

---
+++ src/cli.py
@@ -0,0 +1,34 @@
+from location import Location
+from player_character import PlayerCharacter
+from npc import Npc
+from item import Item
+
+import click
+
+class Cli:
+    temp_loc ={}
+    def welcome():
+        name = input("Welcome to the Ramptops! We're happy to have you here, but what's your name traveller?")
+        Cli.hello(name)
+    
+    def hello(name):
+        print(f"Welcome {name}")
+
+    #def check- where you are- check for allies and enemies and items--> give appropriate options
+
+    def check():
+        location = PlayerCharacter.current_location()
+        if PlayerCharacter.current_location() == False:
+            temp_loc ={0,0}
+        else:
+            Location.__repr__()
+        Cli.checkLocation(location)
+        
+    
+    def checkLocation(location):
+        #check location for allies/enemies
+            #if present let player know/give option to interact/fight
+        #check location for items
+            #if present let player interCact
+
+    #def options: what do you want to do
