a007dad07164f9d1c159f58802f408ce78721eef
jamescross813 <crossjames813@gmail.com>
2024-02-06 22:47:48 -0600

    typo fix

--- src/npc.py
+++ src/npc.py
@@ -12,7 +12,7 @@ class Npc:
         #need attack points
 
     def __repr_(self):
-        return "This is {0}, an {1}. {2}. Currently health: {3}".format(self.name, self.type, self.description, self.hp)
+        return "This is {0}, an {1}. {2}. Currenty health: {3}".format(self.name, self.type, self.description, self.hp)
 
 
     #def fight
